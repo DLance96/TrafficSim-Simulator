3429a0490928df732f70277e8e4eb6e9e846aec1
Peyton Turner <donaldpeytonturner@gmail.com>
2017-10-25 23:46:27 -0400

    Largely Implement Road, Bucket, Intersection
    Substantial portions remain unfinished. Intersection contains a lot of
    code from Road that hasn’t been redesigned yet, but Brett wanted Road
    for simulation testing.

--- src/Intersection.py
+++ src/Intersection.py
@@ -10,7 +10,7 @@ class Road:
 
     lane_width = 10
 
-    def __init__(self, anchor_corner, length, inbound_lanes, outbound_lanes, orientation, speed_limit):
+    def __init__(self, center, radius, speed_limit):
         """
         :param anchor_corner: [double, double]
         :param length: double
@@ -19,24 +19,13 @@ class Road:
         :param orientatino: double (IN RADIANS!)
         :param speed_limit: int
         """
-        self.anchor = anchor_corner
-        self.length = length
-        self.inbound_lanes = inbound_lanes
-        self.outbound_lanes = outbound_lanes
-        self.lanes = inbound_lanes + outbound_lanes
-        self.width = self.lanes * self.lane_width
-        self.orientation = orientation
+        self.center = center
+        self.radius = radius
         self.speed_limit = speed_limit
-        self.bucket_length = 0.2778 * self.speed_limit * 10
-        self.initial_intersection = None
-        self.terminal_intersection = None
         self.vehicles = []
-        self.bucket_list = self.initialize_buckets(road_length = self.length,
-                                          road_width = self.width,
-                                          bucket_length = self.bucket_length,
-                                          inbound_lanes = self.inbound_lanes,
-                                          outbound_lanes = self.outbond_lanes)
-        self.surface = self.generate_surface()
+        self.adjacent_roads = []
+        self.adjacent_road_orientations = []
+        self.adjacent_road_widths = []
         self.next_locations = [] # Prevents conflicts with cars being moved onto roads between tick and tock.
 
     def tick(self, ticktime_ms):
@@ -57,82 +46,40 @@ class Road:
 
         return
 
-    def initialize_buckets(self, road_length, road_width, bucket_length, inbound_lanes, outbound_lanes):
-
-        number_of_buckets = math.ceil(road_length / bucket_length)
-        bucket_list = []
-
-        for i in range(number_of_buckets):
-
-            if i == 0:
-                head = Bucket(intial_x = i * self.bucket_length, length=bucket_length,
-                              inbound_lanes=inbound_lanes, outbound_lanes=outbound_lanes)
-                tail = head
-                bucket_list.append(head)
-            else:
-                next = Bucket(intial_x = i * self.bucket_length, length=bucket_length,
-                              inbound_lanes=inbound_lanes, outbound_lanes=outbound_lanes)
-                next.set_previous_bucket(tail)
-                tail.set_next_bucket(next)
-                tail = next
-                bucket_list.append(tail)
-
-        return bucket_list
-
-    def generate_surface(self):
-        # Points proceed clockwise around the rectangle from the anchor point
-        # [x, y] formatting
-        first = self.anchor
-        second = [first[0] + self.length * math.cos(self.orientation), first[1] + self.length * math.sin(self.orientation)]
-        third = [second[0] + self.width * math.cos(self.orientation + math.pi / 2), second[1] + self.width * math.sin(self.orientation + math.pi / 2)]
-        fourth = [first[0] + self.width * math.cos(self.orientation + math.pi / 2), first[1] + self.diwth * math.sin(self.orientation + math.pi / 2)]
-        # Reference : https://toblerity.org/shapely/manual.html#polygons
-        return geometry.Polygon([first, second, third, fourth])
-
     # Produces the next intended location of each car.
     def request_next_locations(self, ticktime_ms):
         next_locations = [[vehicle.get_intended_position(ticktime_ms), vehicle] for vehicle in self.vehicles]
         return next_locations
 
     # Takes a local coordinate and returns whether or not it is on the road
-    def is_local_on_road(self, location):
-        location = self.local_to_global_location_conversion(location)
-        return self.surface.contains(geometry.Point(location[0], location[1]))
+    def is_local_in_intersection(self, location):
+        # Checking if something is in a circle centered at the origin is so pleasant
+        result = (math.sqrt(math.pow(location[0], 2) + math.pow(location[1], 2)) < self.radius)
+        return result
 
     # Takes a global coordinate and returns whether or not it is on the road
     def is_global_on_road(self, location):
-        return self.surface.contains(geometry.Point(location[0], location[1]))
+        recast = [location[0] - self.center[0], location[1] - self.center[1]]
+        result = (math.sqrt(math.pow(recast[0], 2) + math.pow(recast[1], 2)) < self.radius)
+        return result
 
     def local_to_global_location_conversion(self, location):
         """Turn a local coordinate into its corresponding global coordinate"""
-
-        x = self.anchor[0] + location[0] * math.cos(self.orientation) + location[1] * math.cos(self.orientation + math.pi / 2)
-        y = self.anchor[1] + location[0] * math.sin(self.orientation) + location[1] * math.sin(self.orientation + math.pi / 2)
-
-        return [x, y]
+        return [location[0] + self.center[0], location[1] + self.center[1]]
 
     def global_to_local_location_conversion(self, location):
         """Turn a global coordinate into its corresponding local coordinate"""
+        return [location[0] - self.center[0], location[1] - self.center[1]]
 
-        # Recenter so that the anchor is the origin
-        relative_x = location[0] - self.anchor[0]
-        relative_y = location[1] - self.anchor[1]
-        # Rotate counterclockwise by the orientation
-        local_x = relative_x * math.cos(-self.orientation) - relative_y * math.sin(-self.orientation)
-        local_y = relative_y * math.cos(-self.orientation) + relative_y * math.sin(-self.orientation)
+    # I love circles so much. They are my best friends. -P
 
-        return [local_x, local_y]
-
-    # Takes a global coordinate and returns which, if any of the neighboring intersections contains that coordinate
+    # Takes a global coordinate and returns which, if any of the neighboring roads contains that coordinate
     def which_neighbor(self, location):
 
-        if self.initial_intersection.is_global_in_intersection(self.local_to_global_location_conversion(location)):
-            return self.initial_intersection
-        elif self.terminal_intersection.is_global_in_intersection(self.local_to_global_location_conversion(location)):
-            return self.terminal_intersection
-        else:
-            raise ValueError("No neighbor contains that location.")
-            return
+        for road in self.adjacent_roads:
+            if road.is_global_on_road(location):
+                return road
+        raise ValueError("No neighbor contains that location.")
 
     # Takes a vehicle and a global location and attempts to relocate the vehicle to that location
     def transfer(self, vehicle, location):
@@ -212,12 +159,3 @@ class Road:
         else:
             raise ValueError("Intersection added to an end other than 'initial' or 'terminal'")
         return
-
-
-
-class Bucket_Iterator:
-    """
-    TODO:
-    add getnext() (which also iterates to the next vehicle)
-    getnext returns Null if there are no vehicle left
-    """
