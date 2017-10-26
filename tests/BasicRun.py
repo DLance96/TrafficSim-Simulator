ae76a814635380812878d0dc7280a6e47aae47b2
btj12 <swimbrettjohnson@gmail.com>
2017-10-26 14:01:58 -0400

    improves test

--- tests/BasicRun.py
+++ tests/BasicRun.py
@@ -1,9 +1,12 @@
 from SimulationController import SimulationController
 from TrafficMap import TrafficMap
 from Road import Road
+from drivers.DriverTemplate import  DriverTemplate
+from vehicles.VehicleTemplate import VehicleTemplate
 
 trafficmap = TrafficMap()
-trafficmap.roadlist.append(Road([200,200],200, 1, 2, 5.5, 200))
+onlyroad = Road([200,200],200, 1, 2, 5.5, 200)
+trafficmap.roadlist.append(onlyroad)
 controller = SimulationController(trafficmap, 20, -1, 60)
-
+onlyroad.spawn(DriverTemplate(),VehicleTemplate(),0)
 controller.run()
