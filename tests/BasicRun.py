e86d4f6fa62f84421f3dc5007ccfda7877d0bfe4
btj12 <swimbrettjohnson@gmail.com>
2017-10-26 01:34:40 -0400

    modifies test to have more visible road

--- tests/BasicRun.py
+++ tests/BasicRun.py
@@ -3,7 +3,7 @@ from TrafficMap import TrafficMap
 from Road import Road
 
 trafficmap = TrafficMap()
-trafficmap.roadlist.append(Road([0,0],10, 1, 1, 0, 200))
+trafficmap.roadlist.append(Road([200,200],200, 1, 2, 5.5, 200))
 controller = SimulationController(trafficmap, 20, -1, 60)
 
 controller.run()
