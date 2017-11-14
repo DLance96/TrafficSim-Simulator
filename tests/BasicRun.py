c6037dce5e808fc3cb4c6a3d371fae2ba3472f5a
btj12 <swimbrettjohnson@gmail.com>
2017-11-14 17:20:30 -0500

    Adds speedo ferarro test

--- tests/BasicRun.py
+++ tests/BasicRun.py
@@ -8,17 +8,24 @@ from src.Road import Road
 from src.Intersection import Intersection
 from src.drivers.DriverTemplate import  DriverTemplate
 from src.vehicles.VehicleTemplate import VehicleTemplate
+from src.drivers.DriverTemplate import SpeedoFerraro
+from src.drivers.DriverTemplate import SlowDriver
+from src.vehicles.VehicleTemplate import Ferrari
 
 trafficmap = TrafficMap()
-onlyroad = Road([200,200], 500, 2, 2, 0, 50, .01)
+onlyroad = Road([200,200], 1000, 2, 2, 0, 50, .01)
 initial_intersection = Intersection(center = (200, 220), radius = 30, speed_limit = 200)
-terminal_intersection = Intersection(center = (700, 220), radius = 30, speed_limit = 200)
+terminal_intersection = Intersection(center = (1200, 220), radius = 30, speed_limit = 200)
 onlyroad.add_neighboring_intersection(initial_intersection, 'initial')
 onlyroad.add_neighboring_intersection(terminal_intersection, 'terminal')
 trafficmap.roadlist.append(onlyroad)
 trafficmap.intersectionlist.append(initial_intersection)
 trafficmap.intersectionlist.append(terminal_intersection)
 controller = SimulationController(trafficmap, 20, 100, 60)
-onlyroad.spawn(VehicleTemplate(), DriverTemplate(), "outbound")
+onlyroad.spawn(Ferrari(), SpeedoFerraro(), "outbound", initx=0, laneno=0)
+onlyroad.spawn(VehicleTemplate(), SlowDriver(), "outbound", initx=500, laneno=0)
+onlyroad.spawn(Ferrari(), SpeedoFerraro(), "inbound", initx=0, laneno=0)
+onlyroad.spawn(VehicleTemplate(), SlowDriver(), "inbound", initx=500, laneno=0)
+
 #onlyroad.spawn(VehicleTemplate(), DriverTemplate(), "inbound")
 controller.run()
